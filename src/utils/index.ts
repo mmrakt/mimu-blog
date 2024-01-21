068bb38522c6231fe7cd052c8a8ec0b6eae7059f
mmrakt <mimuraakkun3@gmail.com>
2024-01-21 22:15:22 +0900

    first commit

---
+++ src/utils/index.ts
@@ -0,0 +1,29 @@
+import dayjs from 'dayjs'
+dayjs().format()
+
+import path from 'path'
+const MONTHS = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+]
+
+export const toTitleCase = (str: string) =>
+  str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  })
+
+export const getMonthName = (date: Date) => MONTHS[new Date(date).getMonth()]
+
+export const getSlugFromPathname = (pathname: string) =>
+  path.basename(pathname, path.extname(pathname))
+export const formatPostDate = (date: string) => dayjs(date).format('YYYY-MM-DD')
