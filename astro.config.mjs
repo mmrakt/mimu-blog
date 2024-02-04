fcee7e512d6eabe9e3cbcbac5905ca2abc143307
mmrakt <mimuraakkun3@gmail.com>
2024-02-04 20:50:11 +0900

    fix: change output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,13 @@ import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
-import markdoc from '@astrojs/markdoc'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 import remarkCodeTitles from 'remark-code-titles'
-import decapCmsOauth from 'astro-decap-cms-oauth'
 import rehypeExternalLinks from 'rehype-external-links'
 
 export default defineConfig(
   /** @type {import('astro').AstroUserConfig} */ {
-    output: 'server',
     site: 'https://astro-ink.vercel.app',
     markdown: {
       syntaxHighlight: 'shiki',
@@ -27,13 +23,11 @@ export default defineConfig(
     },
     integrations: [
       mdx(),
-      markdoc(),
       svelte(),
       tailwind({
         applyBaseStyles: false,
       }),
       sitemap(),
-      decapCmsOauth(),
     ],
     vite: {
       plugins: [],
@@ -46,7 +40,6 @@ export default defineConfig(
         allowNodeBuiltins: true,
       },
     },
-    adapter: vercel(),
     devToolbar: {
       enabled: false,
     },
