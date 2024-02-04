809976ec9c7501e18b101e8dfce9cd7da8df4703
mmrakt <mimuraakkun3@gmail.com>
2024-02-04 21:44:03 +0900

    fix: a11y

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -28,12 +28,22 @@ module.exports = {
         },
       },
       typography: (theme) => ({
+        dark: {
+          css: {
+            a: {
+              color: theme('colors.purple.400'),
+              '&:hover': {
+                color: theme('colors.purple.500'),
+              },
+            },
+          },
+        },
         DEFAULT: {
           css: {
             a: {
-              color: colors.dark.primary,
+              color: theme('colors.purple.700'),
               '&:hover': {
-                color: colors.primary,
+                color: theme('colors.purple.600'),
               },
             },
             blockquote: {
